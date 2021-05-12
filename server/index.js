c615b4809629d06d3e6dc2fa20423eb756130d74
KrzysztofDTomaszewski <krzysztofdtomaszewski@gmail.com>
2021-05-12 21:55:45 +0100

    Back-end fixes

--- server/index.js
+++ server/index.js
@@ -1,25 +1,31 @@
 const express = require("express");
 const app = express();
-const mysql = require('mysql')
+const mysql = require("mysql");
+const cors = require("cors");
 const PORT = 3001;
+
+// Tells 'app' to use Cors to parse user input into express function, which will JSONFY the input.
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
     password: '#10LO12ml16',
-    database: 'PasswordManager'
-})
+    database: 'PasswordManager',
+});
 
 
 //application sends '/', request and result to the app (in this)
 app.post("/addpassword", (req, res) => {
-    const {password, title} = req.body
+    const { password, title } = req.body;
 
-    db.query("INSERT INTO passwords (password, title) VALUES (?,?)", 
-    [password, title,],
+    db.query(
+        "INSERT INTO passwords (password, title) VALUES (?,?)", 
+        [password, title,],
         (err, result) => {
             if (err) {
-                console.log(err)
+                console.log(err);
             } else {
                 res.send("Success");
             }
