a02de945c204365eaa52275c8bdf448d1a77acf4
KrzysztofDTomaszewski <krzysztofdtomaszewski@gmail.com>
2021-05-17 14:28:48 +0100

    MySQL privelage errors fixed, DB now supports back-end communications with Axios@localhost:3000

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,46 @@
 import './App.css';
-
+import { useState } from 'react'
+import Axios from 'axios'
 function App() {
+
+  const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('')
+
+/*
+- className=AddPassword, setPassword && setTitle will be encapsulated through input
+- and parsed into state
+*/
+
+  const addPassword = () => {
+    Axios.post('http://localhost:3001/addpassword', {
+      password: password, 
+      title: title,
+    });
+  };
+  
   return (
     <div className="App">
       <div className="AddingPassword">
-        <input type="text" placeholder="Ex. Password123" />
-        <input type="text" placeholder="Ex. Facebook" />
-        <button> Add Password </button>
+        <input 
+          type="text" 
+          placeholder="Ex. Password123" 
+          onChange={(event) => {
+            setPassword(event.target.value)
+          }}
+        />
+
+        <input 
+          type="text" 
+          placeholder="Ex. Facebook" 
+          onChange={(event) => {
+            setTitle(event.target.value)
+          }}
+        />
+        
+        <button onClick={addPassword}> Add Password </button>
       </div>
+
+
     </div>
   );
 }
