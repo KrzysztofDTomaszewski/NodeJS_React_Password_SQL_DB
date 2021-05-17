9baa5db711def368262f2d65afbde3c5e7b32d00
KrzysztofDTomaszewski <krzysztofdtomaszewski@gmail.com>
2021-05-17 12:43:14 +0100

    Front-end has been added on localhost:3000, with input box

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql')
 const PORT = 3001
 
+// db used for queries for MySQL
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password', // OR password: ''
+    database: 'PasswordManager',
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World');
